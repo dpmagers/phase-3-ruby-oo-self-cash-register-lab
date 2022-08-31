2575d8ddd5774e0a7220e38693f40b635a891554
Daniel Magers <daniel.magers@gmail.com>
2022-08-31 07:35:53 -0500

    commit tree a07e650d0c464d29fae5b9d7104d050d8b8436f7

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,67 @@
+#put in pry
+# create CashRegister class
+# create initialize method 
+# put in attr_accessor for @total
 
+require 'pry'
+
+class CashRegister
+
+    attr_accessor :total, :discount
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+    end
+
+    def add_item(title, price, amount=1)
+        @total += price * amount
+    end
+
+    def apply_discount
+        if discount > 0
+            my_discount = @total * @discount/100
+            @total = total - my_discount
+            "After the discount, the total comes to $#{@total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+end
+binding.pry
+#lib/cash_register.rb
+
+# require 'pry'
+
+# class CashRegister
+
+#     attr_accessor :total, :discount
+#     attr_reader  
+#     attr_writer :dollars
+
+#     def initialize(discount = 0)
+#         @discount = discount
+#         @total = 0
+#         @@items = []
+#     end
+
+#     def add_item(title, price, amount = 1)
+#         @total += price * amount
+#     end
+
+#     def apply_discount
+#         if discount > 0
+#             my_discount = @total * @discount/100
+#             @total = total - my_discount
+#             "After the discount, the total comes to $#{@total}."
+#         else
+#             "There is no discount to apply."
+#         end
+#     end
+
+#     # def items(item, price)
+#     #     @items << item
+#     # end 
+# end
+# binding.pry
